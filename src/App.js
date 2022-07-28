a02367fe01116db5ea6a1ca550cd699c0e63acd3
Rachel Tipton <RACHELMTIPTON@GMAIL.COM>
2022-07-28 10:52:52 -0600

    setting up the weekly planner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from 'react'
+import data from './data.json'
+import ToDoList from './ToDoList'
+import ToDoForm from './ToDoForm'
 
-function App() {
+function App () {
+  const [toDoList, setToDoList] = useState(data)
+
+  //add handleToggle (our state is here, so we're doing this here.)
+  const handleToggle = id => {
+    let mapped = toDoList.map(task => {
+      return task.id === id
+        ? { ...task, complete: !task.complete }
+        : { ...task }
+    })
+    setToDoList(mapped)
+  }
+
+  //add handleFilter
+
+  const handleFilter = () => {
+    let filtered = toDoList.filter(task => {
+      return !task.complete
+    })
+    setToDoList(filtered)
+  }
+
+  const addTask = userInput => {
+    let copy = [...toDoList]
+    copy.push({ id: toDoList.length + 1, task: userInput, complete: false })
+    setToDoList(copy)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='content'>
+      <h1>Weekly Planner</h1>
+      <ToDoList
+        toDoList={toDoList}
+        handleToggle={handleToggle}
+        handleFilter={handleFilter}
+      />
+      <ToDoForm addTask={addTask} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/ToDoList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ToDo from './ToDo'
+
+const ToDoList = ({ toDoList, handleToggle, handleFilter }) => {
+  return (
+    <div>
+      {toDoList.map(todo => {
+        return (
+          <ToDo
+            className='content'
+            todo={todo}
+            handleToggle={handleToggle}
+            handleFilter={handleFilter}
+          />
+        )
+      })}
+      <button style={{ margin: '20px' }} onClick={handleFilter}>
+        Clear Completed
+      </button>
+    </div>
+  )
+}
+
+export default ToDoList
